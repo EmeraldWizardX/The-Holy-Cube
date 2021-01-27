c6b78e7fa66ec61ca3fdbfc1dfabbc0b23ad1196
Red <51935295+EmeraldWizardX@users.noreply.github.com>
2021-01-27 15:37:46 -0500

    Add files via upload

---
+++ The_Holy_CubeMain.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "Common\StepTimer.h"
+#include "Common\DeviceResources.h"
+#include "Content\Sample3DSceneRenderer.h"
+
+// Renders Direct3D content on the screen.
+namespace The_Holy_Cube
+{
+	class The_Holy_CubeMain
+	{
+	public:
+		The_Holy_CubeMain();
+		void CreateRenderers(const std::shared_ptr<DX::DeviceResources>& deviceResources);
+		void Update();
+		bool Render();
+
+		void OnWindowSizeChanged();
+		void OnSuspending();
+		void OnResuming();
+		void OnDeviceRemoved();
+
+	private:
+		// TODO: Replace with your own content renderers.
+		std::unique_ptr<Sample3DSceneRenderer> m_sceneRenderer;
+
+		// Rendering loop timer.
+		DX::StepTimer m_timer;
+	};
+}
